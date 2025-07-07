c3dcf015e81be4505e9f787787fdd100f6625c49
耗子 <i@haozi.net>
2025-07-07 15:28:49 +0800

    fix: remove CleanPath middleware

--- internal/http/middleware/middleware.go
+++ internal/http/middleware/middleware.go
@@ -29,17 +29,16 @@ func NewMiddlewares(log *slog.Logger, session *sessions.Manager) *Middlewares {
 // Globals is a collection of global middleware that will be applied to every request.
 func (r *Middlewares) Globals(mux *chi.Mux) []func(http.Handler) http.Handler {
 	return []func(http.Handler) http.Handler{
+		middleware.Recoverer,
 		//middleware.SupressNotFound(r),// bug https://github.com/go-chi/chi/pull/940
-		sessionmiddleware.StartSession(r.session),
-		middleware.CleanPath,
 		middleware.StripSlashes,
-		middleware.Compress(5),
 		middleware.RequestID,
 		middleware.RealIP,
 		httplog.RequestLogger(r.log, &httplog.Options{
 			Level:             slog.LevelInfo,
 			LogRequestHeaders: []string{"User-Agent"},
 		}),
-		middleware.Recoverer,
+		middleware.Compress(5),
+		sessionmiddleware.StartSession(r.session),
 	}
 }
